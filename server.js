37e14d4af43f37b9e802984a0ff69cca2a5816e3
Flory Ann Evia <floryann.evia@gmail.com>
2020-12-03 07:11:39 -0600

    setting up port

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
 
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
 
 mongoose.connect(
     process.env.MONGODB_URI || '',
@@ -15,3 +25,7 @@ mongoose.connect(
   );
 
 require("./routes/api-routes.js")(app);
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
