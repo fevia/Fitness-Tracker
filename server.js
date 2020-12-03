7037a40e2cfe75dfeb59cf2548a35c2ae97710c6
Flory Ann Evia <floryann.evia@gmail.com>
2020-12-03 07:09:55 -0600

    mongoose connection

--- server.js
+++ server.js
@@ -2,3 +2,16 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
+
+
+mongoose.connect(
+    process.env.MONGODB_URI || '',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
+require("./routes/api-routes.js")(app);
